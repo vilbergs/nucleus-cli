b76aef52c424790f95ce1899c596a778f791092f
Vilberg <vilbergs@gmail.com>
2020-02-12 12:54:37 +0100

    Allow flags and fix component paths

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const { args } = Deno
+import { parse } from "https://deno.land/std/flags/mod.ts";
 import { writeFileStrSync, walkSync, readFileStrSync } from 'https://deno.land/std/fs/mod.ts'
 
 
@@ -7,17 +9,35 @@ import { writeFileStrSync, walkSync, readFileStrSync } from 'https://deno.land/s
  * - Make Component directory if it doesn't exist
  * - Allow module name input as cli flag
  */
-Deno.mkdirSync('./src/Atoms', true)
+enum ComponentType {
+    Atom = 'atom',
+    Molecule = 'molecule',
+    Organism = 'organism'
+}
+
+
+const { t: componentType = 'atom', m: moduleName = 'NewComponent' } = parse(args)
+
+const typeToDirectoryMap = {
+    atom: 'Atoms',
+    molecule: 'Molecules',
+    organism: 'Organisms'
+}
+
+if (!typeToDirectoryMap[componentType]) {
+    throw Error('Component type (-t) must be one of atom | molecule | organism')
+}
 
 for (const { filename, info } of walkSync('./template')) {
     if (info.isFile()) {
         const fileString = readFileStrSync(filename)
         
-        const newFilename = filename.split('/').pop().replace('MODULE_NAME', 'MyModule')
+        const newFilename = filename.split('/').pop().replace('MODULE_NAME', moduleName)
         console.log(newFilename)
 
-        const newFileString = fileString.replace(/%MODULE_NAME%/g, 'MyModule')
+        const newFileString = fileString.replace(/%MODULE_NAME%/g, moduleName).replace(/%MODULE_TYPE%/g, componentType)
         
-       writeFileStrSync(`./src/Atoms/${newFilename}`, newFileString)
+        Deno.mkdirSync(`./src/${typeToDirectoryMap[componentType]}/${moduleName}`, true)
+        writeFileStrSync(`./src/${typeToDirectoryMap[componentType]}/${moduleName}/${newFilename}`, newFileString)
     }
 }
