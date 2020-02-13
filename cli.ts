c596b0275c9e2784f4d66f8932f84cbc61b6b614
Vilberg <vilbergs@gmail.com>
2020-02-13 17:57:07 +0100

    simplyfy inpu args

--- cli.ts
+++ cli.ts
@@ -2,7 +2,9 @@ const { args } = Deno
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { writeFileStrSync, walkSync, readFileStrSync } from 'https://deno.land/std/fs/mod.ts'
 
-const { t: componentType = 'atom', m: moduleName = 'NewComponent' } = parse(args)
+
+const [componentType, moduleName] = parse(args)._
+
 
 const typeToDirectoryMap = {
     atom: 'Atoms',
@@ -19,7 +21,6 @@ for (const { filename, info } of walkSync('./template')) {
         const fileString = readFileStrSync(filename)
         
         const newFilename = filename.split('/').pop().replace('MODULE_NAME', moduleName)
-        console.log(newFilename)
 
         const newFileString = fileString.replace(/%MODULE_NAME%/g, moduleName).replace(/%MODULE_TYPE%/g, componentType)
         
--- src/Atoms/MyModule.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface MyModuleProps {}
-
-const MyModule: React.FC<MyModuleProps> = () => {
-    return <div></div>
-}
-
-export default MyModule
