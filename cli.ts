da0b01f7b700881eeb3cdd59d44c48a9badfa615
Vilberg <vilbergs@gmail.com>
2020-02-14 12:28:59 +0100

    Create modules in .src/ of working directory

--- cli.ts
+++ cli.ts
@@ -1,30 +1,28 @@
-const { args } = Deno
-import { parse } from "https://deno.land/std/flags/mod.ts";
+import { parse } from "https://deno.land/std/flags/mod.ts"
 import { writeFileStrSync, walkSync, readFileStrSync } from 'https://deno.land/std/fs/mod.ts'
+import { __ } from 'https://deno.land/x/dirname/mod.ts'
 
-
-const [componentType, moduleName] = parse(args)._
-
-
+const { __dirname } = __(import.meta);
+const [componentType, moduleName] = parse(Deno.args)._
 const typeToDirectoryMap = {
     atom: 'Atoms',
     molecule: 'Molecules',
     organism: 'Organisms'
 }
 
+const workingDir = Deno.cwd()
+
 if (!typeToDirectoryMap[componentType]) {
-    throw Error('Component type (-t) must be one of atom | molecule | organism')
+    throw Error('Component type must be one of atom | molecule | organism')
 }
 
-for (const { filename, info } of walkSync('./template')) {
+for (const { filename, info } of walkSync(`${__dirname}/template`)) {
     if (info.isFile()) {
         const fileString = readFileStrSync(filename)
-        
         const newFilename = filename.split('/').pop().replace('MODULE_NAME', moduleName)
-
         const newFileString = fileString.replace(/%MODULE_NAME%/g, moduleName).replace(/%MODULE_TYPE%/g, componentType)
         
-        Deno.mkdirSync(`./src/${typeToDirectoryMap[componentType]}/${moduleName}`, true)
-        writeFileStrSync(`./src/${typeToDirectoryMap[componentType]}/${moduleName}/${newFilename}`, newFileString)
+        Deno.mkdirSync(`${workingDir}/src/${typeToDirectoryMap[componentType]}/${moduleName}`, true)
+        writeFileStrSync(`${workingDir}/src/${typeToDirectoryMap[componentType]}/${moduleName}/${newFilename}`, newFileString)
     }
 }
